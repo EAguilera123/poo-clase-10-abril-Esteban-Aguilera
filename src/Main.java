c23867d73266d0b05a8c0e758ef9339f95b9bd0f
patat <e.aguilera04@ufromail.cl>
2025-04-10 09:00:31 -0400

    Mi solución consistira de un programa donde se utilizaran los siguientes metodos para administrar y visualizar elementos de una matriz m x n
    los metodos que utilizaré son y seran creados en el siguiente orden:
    menú, validar opcion, ejecutar opcion.
    
    crear matriz, validar dimensiones, llenar matriz.
    
    mostrar fila, matriz cero.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    void menu() {
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. Inversa");
+        System.out.println("5. División");
+        System.out.println("6. Salir");
     }
+
+    int[][] crearMatriz(int filas, int columnas) {
+
+
+    }
+
+
 }
