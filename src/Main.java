bb1e9cb9709178f522be0dba993e76c3cd3c3720
patat <e.aguilera04@ufromail.cl>
2025-04-10 10:38:50 -0400

    Mi solución consistira de un programa donde se utilizaran los siguientes metodos para administrar y visualizar elementos de una matriz m x n
    los metodos que utilizaré son y seran creados en el siguiente orden:
    menú1, validar opcion1.
    
    crear matriz1, mostrar menu1, validar dimensiones1, llenar matriz1.
    
    mostrar fila, matriz cero.

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,30 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    void mostrarMenu() {
-        System.out.println("1. Suma");
-        System.out.println("2. Resta");
-        System.out.println("3. Multiplicación");
-        System.out.println("4. Inversa");
-        System.out.println("5. División");
-        System.out.println("6. Salir");
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static void mostrarMenu() {
+        System.out.println("1. Crear Matriz");
+        System.out.println("2. Llenar Matriz");
+        System.out.println("3. Mostrar fila");
+        System.out.println("4. Verificar si la matriz es cero");
+        System.out.println("5. Salir");
+        preguntarOpcion();
         validarOpcion();
     }
 
-    void validarOpcion(int opcion, Scanner sc) {
+    private static void validarOpcion(int opcion) {
         switch (opcion) {
-            case 1 -> crearMatriz();
+            case 1 -> crearMatriz(int preguntarFilas, int preguntarColumnas);
             case 2 -> llenarMatriz();
-            case 3 -> ();
-            case 4 -> operacionInversa();
-            case 5 -> operacionDivision();
+            case 3 -> mostrarFila(int preguntarFila);
+            case 4 -> matrizCero();
+            case 5 -> System.out.println("saliendo...");
             default -> System.out.println("Opción invalida");
     }
 
-    void menu() {
+    static void menu() {
             do {
                 mostrarMenu();
             } while (true);
@@ -34,7 +37,7 @@ public class Main {
     return matriz;
     }
 
-    boolean validarDimensiones(int filas, int columnas) {
+    static boolean validarDimensiones(int filas, int columnas) {
         if (filas <= 0 && columnas <= 0) {
             return true;
         } else {
@@ -42,7 +45,7 @@ public class Main {
         }
     }
 
-    int[][] llenarMatriz(int matriz[][]) {
+    static int[][] llenarMatriz(int matriz[][]) {
 
         Random rand = new Random();
 
@@ -55,8 +58,51 @@ public class Main {
         return matriz;
     }
 
-    void mostrarFila(int matriz[][], int fila) {
+    static void mostrarFila(int matriz[][], int fila) {
 
-        for (int i = 0; i < matriz.length)
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println(matriz[fila -1][i]);
+        }
     }
+
+    static boolean matrizCero(int matriz[][]) {
+        int numero = 0;
+        int cero = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                numero++;
+                if (matriz[i][j] == 0) {
+                    cero++;
+                }
+            }
+        }
+        if (numero/2 < cero) {
+            System.out.println("la matriz es cero");
+        } else {
+            System.out.println("la matriz no es cero");
+        }
+    }
+
+    static int preguntarFila() {
+        System.out.println("ingrese la fila que quiere ver");
+        int fila = scanner.nextInt();
+                return fila;
+    }
+    static int preguntarOpcion() {
+        System.out.println("ingrese la opcion que quiere usar");
+        int opcion = scanner.nextInt();
+        return opcion;
+    }
+    static int preguntarFilas() {
+        System.out.println("ingrese la cantidad de filas de la matriz");
+        int filas = scanner.nextInt();
+        return filas;
+    }
+    static int preguntarColumnas() {
+        System.out.println("ingrese la cantidad de Columnas de la matriz");
+        int columnas = scanner.nextInt();
+        return columnas;
+    }
+
 }
