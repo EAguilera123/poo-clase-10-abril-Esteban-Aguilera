28c213086a582e1ba84c893eb832af24e2a8acae
patat <e.aguilera04@ufromail.cl>
2025-04-10 09:30:47 -0400

    Mi solución consistira de un programa donde se utilizaran los siguientes metodos para administrar y visualizar elementos de una matriz m x n
    los metodos que utilizaré son y seran creados en el siguiente orden:
    menú, validar opcion.
    
    crear matriz, mostrar menu, validar dimensiones, llenar matriz.
    
    mostrar fila, matriz cero.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
-    void menu() {
+    void mostrarMenu() {
         System.out.println("1. Suma");
         System.out.println("2. Resta");
         System.out.println("3. Multiplicación");
         System.out.println("4. Inversa");
         System.out.println("5. División");
         System.out.println("6. Salir");
+        validarOpcion();
+    }
+
+    void validarOpcion(int opcion, Scanner sc) {
+        switch (opcion) {
+            case 1 -> operacionSuma();
+            case 2 -> operacionResta();
+            case 3 -> operacionMultiplicacion();
+            case 4 -> operacionInversa();
+            case 5 -> operacionDivision();
+            default -> System.out.println("Opción invalida");
+    }
+
+    void menu() {
+            do {
+                mostrarMenu();
+            } while (true);
+        }
     }
 
     int[][] crearMatriz(int filas, int columnas) {
-
-
+           int[][] matriz = new int [filas][columnas];
+    return matriz;
     }
 
+    boolean validarDimensiones(int filas, int columnas) {
+        if (filas <= 0 && columnas <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    int[][] llenarMatriz(int matriz[][]) {
 
+    }
 }
